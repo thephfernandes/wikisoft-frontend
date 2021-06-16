412d5a9f1243ff1708ecbe507eb95ca5d86d9bc8
Oscar Eg Gensmann <oscar@gensmann.dk>
2021-06-16 16:21:53 +0200

    tweaks for vpn

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,21 @@
 module.exports = {
   apps: [
     {
+      name: 'gateway',
+      script: 'server.js',
+      args: 'start',
+      out_file: './logs/out.log',
+      instances: 'max',
+      exec_mode: 'cluster',
+      watch: 'extensions',
+      instance_var: 'INSTANCE_ID',
+      error_file: './logs/err.log',
+      exp_backoff_restart_delay: 100,
+      env: {
+        CONFIG_PATH: 'config.js',
+      },
+    },
+    {
       name: 'directus',
       script: './node_modules/directus/dist/start.js',
       args: 'start',
@@ -23,10 +38,7 @@ module.exports = {
       ref: 'origin/main',
       repo: 'git@github.com:wikisoft-code/io.git',
       path: '/home/node/apps/io',
-      ssh_options: [
-        'ProxyJump=deploy@bastion-server.wikiprofile.com',
-        'IdentityFile=~/.ssh/id_rsa_devops',
-      ],
+      ssh_options: ['IdentityFile=~/.ssh/id_rsa_devops'],
       'post-setup': 'npm run cluster:delete',
       'post-deploy': 'npm install && npm run cluster:start',
     },
