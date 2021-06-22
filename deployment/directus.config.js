57c4e79f07c90eb4ac5a9bdaa9207261c2dc5e35
Oscar Eg Gensmann <oscar@gensmann.dk>
2021-06-22 23:36:38 +0200

    Cleanup, seperate server.config.js, make it simple

---
+++ deployment/directus.config.js
@@ -0,0 +1,35 @@
+const { readdirSync } = require('fs');
+const $$path = require('path');
+
+module.exports = {
+  apps: [
+    {
+      name: 'directus',
+      script: 'npx directus start',
+      exec_mode: process.env.IO_DIRECTUS_INSTANCES > 1 ? 'cluster' : 'fork',
+      instances:
+        process.env.IO_DIRECTUS_INSTANCES > 1
+          ? process.env.IO_DIRECTUS_INSTANCES
+          : 1,
+      instance_var: 'INSTANCE_ID',
+      out_file: '../logs/directus/out.log',
+      error_file: '../logs/directus/err.log',
+      exp_backoff_restart_delay: 100,
+        env: {
+        CONFIG_PATH: './directus/config.js'
+      }
+    }
+  ],
+  deploy: {
+    production: {
+      user: 'hostmaster',
+      host: ['10.0.0.251'],
+      ref: 'origin/main',
+      repo: 'git@github.com:wikisoft-code/io.git',
+      path: '/home/hostmaster/directus',
+      ssh_options: ['IdentityFile=~/.ssh/id_rsa_devops'],
+      'post-deploy':
+        'npm install && npm start:directus',
+    },
+  },
+};
---
+++ deployment/frontend.config.js
@@ -0,0 +1,40 @@
+const { readdirSync } = require('fs');
+const path = require('path');
+
+// Get ENV Settings
+require('dotenv').config();
+
+module.exports = {
+  apps: [
+    {
+      name: 'frontend',
+      script: 'nuxt',
+      args: process.env.NODE_ENV == 'production' ? 'start' : 'dev',
+      cwd: path.join(__dirname, 'frontend'),
+      exec_mode: process.env.IO_FRONTEND_INSTANCES > 1 ? 'cluster' : 'fork',
+      instances:
+        process.env.IO_FRONTEND_INSTANCES > 1
+          ? process.env.IO_FRONTEND_INSTANCES
+          : 1,
+      instance_var: 'INSTANCE_ID',
+      out_file: '../logs/frontend/out.log',
+      error_file: '../logs/frontend/err.log',
+      exp_backoff_restart_delay: 100,
+      env: {
+        API_URL: process.env.IO_API_URL || 'https://io.wikiprofile.com'
+      },
+    },
+  ],
+  deploy: {
+    production: {
+      user: 'web',
+      host: ['10.0.0.252'],
+      ref: 'origin/main',
+      repo: 'git@github.com:wikisoft-code/io.git',
+      path: '/home/node/apps/io',
+      ssh_options: ['IdentityFile=~/.ssh/id_rsa_devops'],
+      'post-deploy':
+        'npm install && npm run build && npm start',
+    },
+  },
+};
