3e071522411a9d07ecbd159361cf2b253e77cf4d
pedro fernandes <thephfernandes@gmail.com>
2021-07-05 22:48:21 +0200

    bunch of layout responsiveness stuff

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -2,15 +2,26 @@
   <div class="base-layout-outer">
     <WikiSearchResults />
     <WikiNavbarTest />
-    <WikiNavbarInternal class="internal-nav" />
-    <div class="base-layout-content columns my-0">
+    <div class="base-layout-content">
+      <div class="columns">
+        <div class="colum is-one-fifth">
+          <WikiSidebarPrimary />
+        </div>
+
+        <div class="column">
+          <nuxt></nuxt>
+        </div>
+      </div>
+    </div>
+    <!-- <WikiNavbarInternal class="internal-nav" /> -->
+    <!-- <div class="base-layout-content columns my-0">
       <div class="column base-layout__sidebar is-one-fifth pr-0 pt-1">
         <WikiSidebarPrimary />
       </div>
       <div class="column pt-1 pl-0 base-layout__content">
         <nuxt class="main-content mt-3 px-3" />
       </div>
-    </div>
+    </div> -->
     <WikiFooterPrimary />
   </div>
 </template>
@@ -47,6 +58,17 @@ export default {
 }
 
 .base-layout-content {
+
+  @include mobile {
+    margin-top: 75px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  @include desktop {
+    margin-top: 95px;
+  }
   // @include desktop {
   //   margin-top: 2rem;
   // }
@@ -62,6 +84,9 @@ export default {
   }
 }
 
+.columns {
+  max-width: 100vw;
+}
 .main-content {
   position: relative;
   // background: linear-gradient(180deg, #ffffff 0%, #e9ebef 100%);
