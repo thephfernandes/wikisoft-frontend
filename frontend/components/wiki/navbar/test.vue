8d06b53bcd86d67328c7cf2888b55eaecaf8939d
pedro fernandes <thephfernandes@gmail.com>
2021-07-05 16:25:38 +0200

    temp

---
+++ frontend/components/wiki/navbar/test.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      <header>
+          <WikiProfilePhoto />
+          <b-field></b-field>
+          <WikiIconWicon icon="burger" />
+      </header>
+      <nav></nav>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
