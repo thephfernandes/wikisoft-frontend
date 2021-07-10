36a8ea08087c547f15921770bef6239e3dc6386f
Oscar Eg Gensmann <oscar@gensmann.dk>
2021-07-10 02:48:02 +0200

    shell files

--- directus/extensions/endpoints/search/index.js
+++ directus/extensions/endpoints/search/index.js
@@ -1,12 +1,19 @@
-// extensions/endpoints/recipes/index.js
-
 module.exports = function registerEndpoint(router, { services, exceptions }) {
   const { ItemsService } = services;
   const { ServiceUnavailableException } = exceptions;
 
-  router.get("/", search);
+  router.get("/", redirect);
+  router.get("/:type/", search);
+  router.get("/:type/:query/", search);
+  router.get("/:type/:query/:sort/", search);
+  router.get("/:type/:query/:sort/:filter/", search);
+  router.get("/:type/:query/:sort/:filter/:page/", search);
+
+  function redirect(req, res, next) {
+    res.json({ success: "ok" });
+  }
 
   function search(req, res, next) {
-    res.json({ success: "ok" });
+    res.json(res.json(req.params));
   }
 };
