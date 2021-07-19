0afc8154501c21e43f7935f6f5490a1326eabaa9
pedro fernandes <thephfernandes@gmail.com>
2021-07-19 17:39:01 -0300

    get google contacts

--- frontend/components/wiki/google/ContactInvite.vue
+++ frontend/components/wiki/google/ContactInvite.vue
@@ -1,18 +1,82 @@
 <template>
   <div>
-    <WikiHeaderPrimary :size="3" :semantic="3">
-      Invite your google contacts to Wikiprofile
+    <WikiHeaderPrimary :size="1" :semantic="3">
+      Invite your contacts to Wikiprofile
     </WikiHeaderPrimary>
-    <figure></figure>
-    <WikiButtonBased>
-      <span class="is-uppercase">invite your network</span>
+    <WikiButtonBased @click="isInviteContactsActive = true">
+      <span class="icon"></span>
+      <span class="text">Invite your network</span>
     </WikiButtonBased>
+
+    <b-modal has-modal-card trap-focus v-model="isInviteContactsActive">
+      <div class="modal-card">
+        <div class="modal-card-body">
+          <WikiHeaderPrimary :size="3" :semantic="1">
+            Invite your google contacts to join Wikiprofile
+          </WikiHeaderPrimary>
+        </div>
+
+        <div class="google-contacts-list">
+          <div class="google-contacts-list__item" v-for="(item, i) in uninvitedGoogleContacts" :key="i">
+            <div class="google-contact">
+              {{ item }}
+            </div>
+            <button>invite</button>
+          </div>
+        </div>
+      </div>
+    </b-modal>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      isInviteContactsActive: false,
+      googleContacts: [],
+    }
+  },
+
+  computed: {
+    uninvitedGoogleContacts() {
+      return this.googleContacts;
+    }
+  },
+
+  async mounted() {
+    const gapi = await this.$gapi.getGapiClient();
+    const res = await gapi.client.people.people.connections.list({
+      'resourceName': 'people/me',
+      'personFields': 'names'
+    });
+    if (res.body.connections) {
+      this.googleContacts = res.body.connections;
+    }
+  }
+}; 
 </script>
 
-<style>
+<style lang="scss" scoped>
+.icon {
+  background-image: url(https://developers.google.com/identity/images/g-logo.png);
+  background-size: 26px 26px;
+  background-repeat: no-repeat;
+  background-position: center center;
+  display: inline-block;
+  vertical-align: middle;
+  padding-left: 8px;
+  width: 42px;
+  height: 42px;
+}
+
+.text {
+  display: inline-block;
+  vertical-align: middle;
+  padding-left: 24px;
+  padding-right: 24px;
+  font-size: 14px;
+  font-weight: bold;
+  font-family: "Roboto", sans-serif;
+}
 </style>
--- frontend/plugins/vueGapi.js
+++ frontend/plugins/vueGapi.js
@@ -4,7 +4,7 @@ import VueGapi from 'vue-gapi';
 const config = {
   apiKey: process.env.GOOGLE_API_KEY,
   clientId: process.env.GOOGLE_CLIENT_ID,
-  scope: 'profile',
+  scope: 'https://www.googleapis.com/auth/contacts.readonly',
   discoveryDocs: ['https://people.googleapis.com/$discovery/rest?version=v1']
 }
 
