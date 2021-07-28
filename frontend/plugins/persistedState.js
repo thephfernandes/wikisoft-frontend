a2f795ce9655ae12779323a16547b8f8f25cb901
pedro fernandes <thephfernandes@gmail.com>
2021-07-28 15:26:01 -0300

    clean up plugin

--- frontend/plugins/persistedState.js
+++ frontend/plugins/persistedState.js
@@ -1,26 +1,26 @@
 import createPersistedState from 'vuex-persistedstate';
 import * as Cookies from 'js-cookie';
+import cookie from 'cookie';
 
-export default ({ app, store, req }) => {
+export default ({ store, req }) => {
   createPersistedState({
-    paths: ['user', 'companies', 'people'],
-    storage: {
-      getItem: (key) => {
-        if (process.server) {
-          let cookieHeader = req.headers.cookie;
-          if (typeof cookieHeader !== 'string') {
-            cookieHeader = '';
-          }
-          const parsedCookies = app.$cookies.nodeCookie.parse(cookieHeader);
-          return parsedCookies[key];
-        } else {
-          return Cookies.get(key);
-        }
-      },
-      setItem: (key, value) =>
-        Cookies.set(key, value, { secure: false }),
-
-      removeItem: key => Cookies.remove(key)
-    }
+    paths: ['user', 'companies', 'people', 'oAuth'],
+    // storage: {
+    //   // getItem: (key) => {
+    //   //   if (process.server) {
+    //   //     let cookieHeader = req.headers.cookie;
+    //   //     if (typeof cookieHeader !== 'string') {
+    //   //       cookieHeader = '';
+    //   //     }
+    //   //     const parsedCookies = cookie.parse(cookieHeader);
+    //   //     return parsedCookies[key];
+    //   //   } else {
+    //   //     return Cookies.get(key);
+    //   //   }
+    //   // },
+    //   getItem: (key) => process.client ? Cookies.getJSON(key) : cookie.parse(req.headers.cookie||'')[key],
+    //   setItem: (key, value) => Cookies.set(key, value, { secure: false }),
+    //   removeItem: key => Cookies.remove(key)
+    // }
   })(store);
 };
