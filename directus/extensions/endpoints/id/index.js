95a9a9e45d05ec4b0c18671ad14f813629b365b0
BenjaminRasmussen <dumstick228@gmail.com>
2021-08-03 15:31:51 +0200

    Update index.js

--- directus/extensions/endpoints/id/index.js
+++ directus/extensions/endpoints/id/index.js
@@ -12,6 +12,7 @@ module.exports = function registerEndpoint(router, { services, exceptions, datab
   function get_item(req, res, next) {
     database.raw(
       `select (id, "weight-a", "weight-b", "weight-c", content) from search.items where id = '${req.params.id}'`
-    ).then((results) => res(results.rows[0].row)).catch(err => res.json(req.params))
+    ).then((results) => { return results.rows[0].row }
+    ).catch(err => res.json(req.params))
   }
 };
